9fdc99bfab2370a883e1d44f76c6c7b31fc14036
Alan Fleming <alanfleming1998@gmail.com>
2020-02-03 12:19:34 -0500

    added resources to sim_1

--- scripts/sim_1.py
+++ scripts/sim_1.py
@@ -7,8 +7,14 @@ def example(env):
     value = yield event
     print('now=%d, value=%d' % (env.now, value))
 
+#setup env
 env = simpy.Environment()
 example_gen = example(env)
+#setup resources
+orderA = simpy.Resource(env capacity=1)
+orderB = simpy.Resource(env capacity=1)
+pickup = simpy.Resource(env capacity=1)
+#create the process
 p = simpy.events.Process(env, example_gen)
-
+#run the env
 env.run()

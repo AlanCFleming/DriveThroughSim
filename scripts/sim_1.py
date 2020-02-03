03300645a400acb42ae133df70a7ccb5a4481eaa
Alan Fleming <alanfleming1998@gmail.com>
2020-02-03 13:54:18 -0500

    removed inline comments added wait time to orders

--- scripts/sim_1.py
+++ scripts/sim_1.py
@@ -17,15 +17,19 @@ def example(env):
 def car(env, number, orderA, orderB, pickup):
     arrival_time = env.now() #gets arrival time
 
-    #checks if the car leaves or stays
-    if(len(orderA.queue) > 3 and len(orderB.queue) > 3) # checks if both order windows are full. 4 people waiting is full.
-        print("%7.4f: %s left without ordering" % (env.now, number)); #prints time the car left
+    #checks if the car leaves or stays due to line length
+    if(len(orderA.queue) > 3 and len(orderB.queue) > 3) 
+        #prints the time the car left
+        print("%7.4f: %s left without ordering" % (env.now, number)); 
     else
-        line = orderA if (len(orderA.queue) < len(orderB.queue)) else orderB #assigns the shortest line to the car
+        #assigns the shortest line to the car
+        line = orderA if (len(orderA.queue) < len(orderB.queue)) else orderB 
         
-        #make the car join the line it picked
-        yield line.request # wait until it gets to the oder window
+        #make the car join the line it picked and wait until it gets to order
+        yield line.request 
+        #random number for wait time
         order_time = random.expovariate(1.0 / mean_order_time) 
+        yield env.timeout(order_time)
         line.release(line)
 
 #setup env

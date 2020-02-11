af1bd8fee04480096096aa41fba6952765fd1dff
Alan Fleming <alanfleming1998@gmail.com>
2020-02-11 13:24:46 -0500

    added code to test lowering ar of sim_1 until more cars are leaving than staying

--- scripts/sim_1.py
+++ scripts/sim_1.py
@@ -12,38 +12,35 @@ mean_order_time = 2
 mean_prep_time = 5
 mean_collect_time = 2
 
-mean_AR = 1.5
+mean_AR = 5
 
-#count of cars total and cars left
-count = 0
-left = 0
-
-def cargen(env, time, orderA, orderB,lineP, pickup):
-    #access global variable
-    global count
+def cargen(env, time, orderA, orderB,lineP, pickup, running, count, left):
     
     #generate cars during time frame
     while(time >= env.now):
         #make and start a car
-        count += 1
-        c = car(env, count, orderA, orderB,lineP, pickup)
+        count.put(1)
+        running.put(1)
+        c = car(env, count.level, orderA, orderB,lineP, pickup, running, left)
         env.process(c)
         #wait to make another
         t = t = random.expovariate(1.0 / mean_AR)
         yield env.timeout(t)
+    
+    while(running.level>  0):
+        yield env.timeout(1)
 
 
 #car to go through the line
-def car(env, number, orderA, orderB, lineP, pickup):
-    arrival_time = env.now #gets arrival time
-
-    #access global variable
-    global left
+def car(env, number, orderA, orderB, lineP, pickup, running, left):
+    #gets arrival time
+    arrival_time = env.now
 
     #checks if the car leaves or stays due to line length.
     if(len(orderA.queue) >  order_length and len(orderB.queue) > order_length): 
-        print("%7.4f: %s left without ordering" % (env.now, number)) 
-        left += 1
+        #print("%7.4f: %s left without ordering" % (env.now, number)) 
+        left.put(1)
+        running.get(1)
     else:
         #assigns the shortest line to the car
         lineA = len(orderA.queue) < len(orderB.queue) 
@@ -87,12 +84,13 @@ def car(env, number, orderA, orderB, lineP, pickup):
             yield env.timeout(prep_time - env.now)
         pickup.release(line)
 
+        #take running resource
+        running.get(1)
+
         #print time taken 
-        print("%7.4f: %s left after %s minutes" % (env.now, number, env.now - arrival_time))
+        #print("%7.4f: %s left after %s minutes" % (env.now, number, env.now - arrival_time))
 
-
-
-#setup env
+#setup environment
 env = simpy.Environment()
 #setup resources
 ##ordering recources
@@ -101,12 +99,30 @@ orderB = simpy.Resource(env, capacity=1)
 ##pickup resources
 pickup = simpy.Resource(env, capacity=1)
 lineP = simpy.Resource(env, capacity=6)
-#create the process
-generator = cargen(env,120 ,orderA ,orderB,lineP, pickup)
-p = simpy.events.Process(env, generator)
-#seed the random number gen
-random.seed(random_seed)
-#run the env
-env.run()
-#print stats
-print(count,left)
+#setup counting resources
+running = simpy.Container(env)
+count = simpy.Container(env)
+left = simpy.Container(env)
+#first run boolean
+first = True
+#Run until more than 50% of cars leave
+while(first or (left.level/count.level < .5)):
+    #clear the first run boolean 
+    first = False
+
+    #clear counting resources
+    running = simpy.Container(env)
+    count = simpy.Container(env)
+    left = simpy.Container(env)
+
+    #create the process
+    generator = cargen(env, (env.now + 120) ,orderA ,orderB,lineP, pickup, running, count, left)
+    p = simpy.events.Process(env, generator)
+    #seed the random number gen
+    random.seed(random_seed)
+    #run the env
+    env.run()
+    #print stats
+    print(mean_AR, count.level, left.level, left.level / count.level )
+    #lower AR
+    mean_AR = mean_AR - 0.125
